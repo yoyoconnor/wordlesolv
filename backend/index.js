c7beda65c5832a0f1ec13e32dc536781dbd0b938
Connor <connorbarbaccia@gmail.com>
2023-08-23 01:02:50 -0500

    add production sit to allowed orgins

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'vercel.app'); // Replace * with your specific origin(s)
+    res.setHeader('Access-Control-Allow-Origin', 'https://wordlesolver-six.vercel.app '); // Replace * with your specific origin(s)
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
