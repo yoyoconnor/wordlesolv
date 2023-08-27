5b0dec593cfe9fae3bf1f40fc4db6040e1f2ed32
Connor <connorbarbaccia@gmail.com>
2023-08-27 00:55:45 -0500

    add cors support

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const solver = require('./solver.js');
 const express = require('express');
+const cors = require('cors');
 
 const app = express();
 const port = process.env.PORT || 3001;
+
+app.use(cors());
 
 app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', 'https://wordlesolver-six.vercel.app '); // Replace * with your specific origin(s)
